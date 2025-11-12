376edd3fd669c157dd7a38aff5e71fa7fb1a99b6
xiuc <ccuix@qq.com>
2025-11-12 23:49:36 +0800

    toggleterm unlazy load
    - unlazy load toggleterm
    - rebind toggleterm open_mapping , because <c-\> is confilting with
    [<c-\><c-n>]

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,11 +1,11 @@
-
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
+  lazy = false,
   config = function()
     require("toggleterm").setup{
       size = 12,
-      open_mapping = [[<c-\>]],
+      open_mapping = [[<c-t>]],
       direction = 'horizontal',
       shade_terminals = true,
       start_in_insert = true,
